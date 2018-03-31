62f0d3c9461dcf37880e6bdeb86bbf9936f22ec3
Atharvacc <atharvanchalke@gmail.com>
2018-03-31 09:08:43 -0500

    New changes
    New changes

---
+++ Test.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Hello world </h1>
+
+<button type = "button"
+onclick = "document.getElementById('demo').innerHTML=Date()">
+Click Me to show Date and time . </button>
+
+<p id = "demo"></p>
+
+</body>
+</html>
